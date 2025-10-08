ee5c210e82b41f85fa98e295983f57ead8b12413
Ali Baghernia <alibaghernia0@gmail.com>
2025-10-08 10:33:10 +0330

    g

---
+++ _next/static/chunks/9fe9ca487129924e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/a32c6964490e0955.js",
+  "static/chunks/turbopack-2b23b0e3aaa3ece5.js"
+])
---
+++ _next/static/chunks/e5aaa26230ec6b83.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a32c6964490e0955.js",
+  "static/chunks/turbopack-5ff7df4b38aa937f.js"
+])
